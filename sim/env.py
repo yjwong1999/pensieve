5f0b3a202a0a948fea43824608c80aa93d16998c
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-10-12 03:56:30 +0800

    Update env.py

--- sim/env.py
+++ sim/env.py
@@ -88,8 +88,8 @@ class Environment:
         delay *= MILLISECONDS_IN_SECOND
         delay += LINK_RTT
 
-	# add a multiplicative noise to the delay
-	delay *= np.random.uniform(NOISE_LOW, NOISE_HIGH)
+        # add a multiplicative noise to the delay
+        delay *= np.random.uniform(NOISE_LOW, NOISE_HIGH)
 
         # rebuffer time
         rebuf = np.maximum(delay - self.buffer_size, 0.0)
