99b8cdb1bb3a3a785ca1a328e224da5418b943e4
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-10-13 10:01:58 +0200

    Update mpc_future_bandwidth.py

--- test/mpc_future_bandwidth.py
+++ test/mpc_future_bandwidth.py
@@ -1,7 +1,6 @@
 import numpy as np
-import fixed_env2 as env
+import fixed_env as env
 import load_trace
-import matplotlib.pyplot as plt
 import itertools
 
 
@@ -264,16 +263,15 @@ def main():
             a_batch.append(action_vec)
             entropy_record = []
 
-            print "video count", video_count
+            print("video count", video_count)
             video_count += 1
 
             if video_count >= len(all_file_names):
                 break
 
             log_path = LOG_FILE + '_' + all_file_names[net_env.trace_idx]
-            log_file = open(log_path, 'wb')
+            log_file = open(log_path, 'w')
 
 
 if __name__ == '__main__':
     main()
-
