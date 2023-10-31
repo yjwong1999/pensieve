510036b10e8cf832253ef8ead2206261bdfb1846
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-10-31 10:58:30 +0100

    Update get_video_sizes.py

--- test/get_video_sizes.py
+++ test/get_video_sizes.py
@@ -10,14 +10,15 @@ VIDEO_FOLDER = 'video'
 # the quality at video5 is the lowest and video1 is the highest
 
 
-for bitrate in xrange(BITRATE_LEVELS):
-	with open('video_size_' + str(bitrate), 'wb') as f:
-		for chunk_num in range(1, TOTAL_VIDEO_CHUNCK+1):
-			video_chunk_path = VIDEO_PATH + \
-							   VIDEO_FOLDER + \
-							   str(BITRATE_LEVELS - bitrate) + \
-							   '/' + \
-							   str(chunk_num) + \
-							   '.m4s'
-			chunk_size = os.path.getsize(video_chunk_path)
-			f.write(str(chunk_size) + '\n')
+for bitrate in range(BITRATE_LEVELS):
+    with open('video_size_' + str(bitrate), 'wb') as f:
+        for chunk_num in range(1, TOTAL_VIDEO_CHUNCK+1):
+            video_chunk_path = VIDEO_PATH + \
+                                VIDEO_FOLDER + \
+                                str(BITRATE_LEVELS - bitrate) + \
+                                '/' + \
+                                str(chunk_num) + \
+                                '.m4s'
+            chunk_size = os.path.getsize(video_chunk_path)
+            msg = str(chunk_size) + '\n'
+            f.write(msg.encode('utf-8'))
