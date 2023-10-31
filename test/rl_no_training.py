f9852a04e9f0f3844a658b5482ced731766b69c1
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-10-31 11:12:36 +0100

    Update rl_no_training.py

--- test/rl_no_training.py
+++ test/rl_no_training.py
@@ -100,13 +100,14 @@ def main():
             last_bit_rate = bit_rate
 
             # log time_stamp, bit_rate, buffer_size, reward
-            log_file.write(str(time_stamp / M_IN_K) + '\t' +
-                           str(VIDEO_BIT_RATE[bit_rate]) + '\t' +
-                           str(buffer_size) + '\t' +
-                           str(rebuf) + '\t' +
-                           str(video_chunk_size) + '\t' +
-                           str(delay) + '\t' +
-                           str(reward) + '\n')
+            msg = str(time_stamp / M_IN_K) + '\t' + \
+                           str(VIDEO_BIT_RATE[bit_rate]) + '\t' + \
+                           str(buffer_size) + '\t' + \
+                           str(rebuf) + '\t' + \
+                           str(video_chunk_size) + '\t' + \
+                           str(delay) + '\t' + \
+                           str(reward) + '\n'
+            log_file.write(msg.encode('utf-8'))
             log_file.flush()
 
             # retrieve previous state
